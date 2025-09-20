192c6f611383c15653afc5991b61275e173f207c
Nastawnick <lev.tn2005@yandex.ru>
2025-09-20 18:30:27 +0500

    Test 10 Архитектура, стили и header

--- App/FrontEnd/monitor/interface/urls.py
+++ App/FrontEnd/monitor/interface/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about-us', views.about)
+    path('', views.main, name = "home"),
+    path('my_sites', views.my_sites, name = "my_sites"),
+    path('account', views.account, name = "account")
 ]
