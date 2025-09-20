6e6072e3f2ab8c858edde66f70bf6b0873144430
Pavel <pavelnovokreshenov2017@mail.ru>
2025-09-20 15:41:01 +0500

    upd

--- App/main.py
+++ App/main.py
@@ -1,10 +1,13 @@
+import asyncio
+
 from App.BackEnd.Services.StatusChecking.StatusHTTP import request_check
 from App.BackEnd.Services.StatusChecking.DNSCheck import resolve_url
 
 def main():
     url_check = 'https://www.djangoproject.com/download/'
+    resolver = asyncio.run(resolve_url(url_check))
     print(request_check(url_check))
-    print(resolve_url(url_check))
+    print(resolver)
 
 
 if __name__ == "__main__":
