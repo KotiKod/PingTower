e6331c44850e6647c1a36def2af4dc7d1bf1fab9
Pavel <pavelnovokreshenov2017@mail.ru>
2025-09-20 17:22:13 +0500

    upd

---
+++ App/BackEnd/Services/StatusChecking/webdriverCheck.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import time
+
+def webdriver_check(url):
+    options = Options()
+    options.add_argument('--headless')
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service, options=options)
+
+    start = time.time()
+    driver.get(url)
+    end = time.time()
+    return {"time_to_load": round(end-start, 2)}
+
+def main():
+    url = "https://www.google.com"
+    print(webdriver_check(url))
+
+if __name__ == "__main__":
+    main()
